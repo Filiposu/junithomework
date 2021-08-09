a503fc958d92d73f8399737a0eed22f7d0fc4a5a
Mahmud Aliyev <mahmud.aliyev@ibar.az>
2021-08-09 17:03:28 +0400

    added swagger, controller with hello method and removed unused dependencies

--- src/main/java/az/ibar/ms/template/TemplateApplication.java
+++ src/main/java/az/ibar/ms/template/TemplateApplication.java
@@ -2,10 +2,8 @@ package az.ibar.ms.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class TemplateApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/az/ibar/ms/template/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package az.ibar.ms.template.config;
+
+import java.util.Set;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = java.util.Set.of(MediaType.APPLICATION_JSON_VALUE);
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("az.ibar.ms.template.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+}
